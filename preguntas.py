9eab04ae416f76922e41d51bd02dd1af56944c78
jafernandezca <160281683+jafernandezca@users.noreply.github.com>
2024-03-07 21:34:19 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -157,7 +157,7 @@ def pregunta_09():
     39   39   E    5  1998-01-26  1998
 
     """
-    tbl0['year'] = pd.to_datetime(tbl0['_c3'], errors='coerce').dt.year.astype(int)
+    tbl0['year'] = pd.to_datetime(tbl0['_c3'], errors='coerce').dt.year.fillna(0).astype(int)
     return tbl0
 
 def pregunta_10():
