fd12c85f4ae083d2b4f900665fd7eff707381066
jafernandezca <160281683+jafernandezca@users.noreply.github.com>
2024-03-07 21:45:58 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -157,8 +157,10 @@ def pregunta_09():
     39   39   E    5  1998-01-26  1998
 
     """
-    tbl0['year'] = pd.to_datetime(tbl0['_c3'], errors='coerce').dt.year.fillna(0).astype(int)
+    tbl0['year'] = pd.to_datetime(tbl0['_c3'], errors='coerce').dt.year
+    tbl0['year'] = tbl0['year'].astype('Int64')  # Convertir a entero, maneja NA correctamente
     return tbl0
+
 
 def pregunta_10():
     """
